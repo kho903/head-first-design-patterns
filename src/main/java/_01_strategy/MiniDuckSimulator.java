1cdcde3b7aa791fd5967a48b6f1ee05ea2ec8093
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-03 15:11:58 +0900

    feat: 전략 패턴 - 동적으로 행동 지정하기
    - Duck 클래스에 setter 추가 (setFlyBahavior(), seatQuackBehavior())
    - ModelDuck 서브 클래스
    - FlyRocketPowered

--- src/main/java/_01_strategy/MiniDuckSimulator.java
+++ src/main/java/_01_strategy/MiniDuckSimulator.java
@@ -7,5 +7,14 @@ public class MiniDuckSimulator {
 		// 이 메소드에서는 객체의 QuackBehavior, FlyBehavior에게 할 일을 위임한다.
 		mallard.performQuack();
 		mallard.performFly();
+
+		Duck model = new ModelDuck();
+		model.performFly(); // performFly() 처음 호출시 ModelDuck 생성자에서 설정되었떤 flyBehavior,
+							// 즉, flyNoWay 인스턴스의 fly() 메소드가 호출된다.
+		model.setFlyBehavior(new FlyRocketPowered()); // 상속받은 행동 세터 메소드가 호출된다.
+													  // 이제 모형 오리에 로켓 추진력으로 날 수 있는 능력이 생겼다.
+		model.performFly(); // 제대로 돌아간다면 모형 오리는 동적으로 나는 행동을 바꿀 수 있다.
+							// 구현 코드가 Duck 클래스 안에 들어있다면 그렇게 할 수 없었다.
+
 	}
 }
